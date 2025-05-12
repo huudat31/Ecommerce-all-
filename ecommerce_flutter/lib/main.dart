d8130bda30aa6ba2cb19fccc7a86ed2b1c32df7e
huudat31 <phanhuudat310708@gmail.com>
2025-05-12 14:49:21 +0700

    add splash_screen

---
+++ ecommerce_flutter/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:ecommer_all/screens/splash_screen/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(
+        body: Center(
+          child: SplashScreen(),
+        ),
+      ),
+    );
+  }
+}
